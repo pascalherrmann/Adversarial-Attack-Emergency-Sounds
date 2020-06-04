9f686d72f11b396201b0c7bfdceb96141ffa7609
Yan Scholten <yascho@mail.upb.de>
2020-06-04 20:34:58 +0200

    YS

---
+++ src/attacks/fgsm.py
@@ -0,0 +1,17 @@
+from attacks.attack import Attack
+import torch.nn.functional as F
+
+class FGSM(Attack):
+    
+    def attackSample(self, model, x, y, epsilon, num_iter=1):
+        x.requires_grad = True
+        
+        loss = F.nll_loss(model(x), y)
+        model.zero_grad()
+        loss.backward()
+        
+        perturbed_sample = x + epsilon * x.grad.data.sign()
+        return perturbed_sample
+
+if __name__ == '__main__':
+    pass
