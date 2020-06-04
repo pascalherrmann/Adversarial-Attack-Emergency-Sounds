19948c787662562f55a9804052a04cc416725654
Yan Scholten <yascho@mail.upb.de>
2020-06-04 21:40:48 +0200

    YS

---
+++ src/attacks/pgd.py
@@ -0,0 +1,18 @@
+from attacks.attack import Attack
+import torch.nn.functional as F
+
+class PGD(Attack):
+    
+    def attackSample(self, x, y, epsilon=0, num_iter=1):
+        for i in range(num_iter):
+            x.requires_grad_()
+            loss = F.nll_loss(self.model(x), y)
+            model.zero_grad()
+            loss.backward()
+
+            x = x + epsilon * x.grad.data
+
+            # projection in case epsilon is too large
+            x = x.clamp(-1, 1).detach() 
+
+        return x 
