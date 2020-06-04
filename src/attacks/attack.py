175a99720b837c8f0317688e9a4204de59ea3426
Yan Scholten <yascho@mail.upb.de>
2020-06-04 20:54:34 +0200

    YS

--- src/attacks/attack.py
+++ src/attacks/attack.py
@@ -5,9 +5,10 @@ from tqdm import tqdm
 
 class Attack(ABC):
     
-    def __init__(self, model, data_loader, early_stopping=-1):
+    def __init__(self, model, data_loader, attack_parameters, early_stopping=-1):
         self.model = model
         self.data_loader = data_loader
+        self.attack_parameters = attack_parameters
         self.early_stopping = early_stopping # -1=disabled 
 
         assert self.data_loader.batch_size == 1
@@ -17,7 +18,8 @@ class Attack(ABC):
         self.total = 0
         self.adversarial_examples = []
         
-    def attack(self, attack_parameters):
+    def attack(self):
+        assert self.total == 0 # only attack once
 
         for i, data in tqdm(list(enumerate(self.data_loader,0)), position=0):
             x, y_true = [x.cuda() for x in data]
@@ -26,7 +28,7 @@ class Attack(ABC):
             if y_initial != y_true:
                 continue # we only attack correctly classified samples (TPs and TNs)  
 
-            x_perturbed = self.attackSample(x, y_true, **attack_parameters)
+            x_perturbed = self.attackSample(x, y_true, **self.attack_parameters)
             y_perturbed = self.predictClass(x_perturbed)
 
             self.evaluateAttack(i, y_perturbed, y_initial)
