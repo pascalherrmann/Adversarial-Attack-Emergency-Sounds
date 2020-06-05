f1e43fd370f2a36f039c3216b728f52933513b50
Yan Scholten <yascho@mail.upb.de>
2020-06-06 01:04:50 +0200

    pitch attack

--- src/attacks/pitch.py
+++ src/attacks/pitch.py
@@ -2,10 +2,11 @@ from attacks.attack import Attack
 
 from torchaudio import functional as AF
 import torch.nn.functional as F
+import torchaudio
 import math
 import torch
 
-class TimeStretchAttack(Attack):
+class PitchAttack(Attack):
     
     """
         lower: lower bound for streching rate 
@@ -14,22 +15,35 @@ class TimeStretchAttack(Attack):
         stretching_rate > 1 means speedup
         stretching_rate > 1 means slowdown
     """
-    def attackSample(self, x, y, num_iter=1, lower=0.9, upper=1.1):
-        rate_search_range = torch.arange(lower, upper, (upper-lower)/num_iter)
+    def attackSample(self, x, y, sr, num_iter=1, lower=1, upper=5):
+        n_steps_search_range = torch.arange(lower, upper, (upper-lower)/num_iter)
         losses = []
         stretched_inputs = []
-
+        
         with torch.no_grad():
-            for rate in rate_search_range:
-                stretched = self.time_stretch(x.squeeze().cpu(), rate)
+            for n_steps in n_steps_search_range:
+                stretched = self.pitch_shift(x.squeeze().cpu(), sr, n_steps)
                 stretched = stretched.cuda().unsqueeze(0)
             stretched_inputs.append(stretched)
             losses.append(F.nll_loss(self.model(stretched), y))
         best_rate = torch.stack(losses).argmax().item()
         return stretched_inputs[best_rate]
 
+    def pitch_shift(self, sample, sr, n_steps, bins_per_octave=12): 
+        # https://librosa.github.io/librosa/_modules/librosa/effects.html#pitch_shift
+        assert bins_per_octave >= 1
+        rate = 2.0 ** (-float(n_steps) / bins_per_octave)
+
+        # Stretch in time, then resample, compare librosa
+        resample = torchaudio.transforms.Resample(float(sr)/rate, sr)
+        y_shift = resample(self.time_stretch(sample, rate)) # not diff'able
+        
+        # back to original size
+        low = int((y_shift.shape[0] - max_length)/2)
+        return y_shift[low:low+max_length]
+
     """
-        Time stretching with padding
+        Time stretching *without* padding
         
         Note this function is **not** differentiable:
         through speedup frames get deleted/inserted, 
@@ -47,16 +61,4 @@ class TimeStretchAttack(Attack):
         phase_advance = torch.linspace(0, math.pi * hop_length, stft.shape[1])[..., None]
         # time stretch via phase_vocoder (not differentiable):
         vocoded = AF.phase_vocoder(stft, rate=speedup_rate, phase_advance=phase_advance) 
-        istft = AF.istft(vocoded, n_fft.item(), hop_length=hop_length).squeeze()
-
-        # padding
-        max_length = sample.shape[0]
-        if speedup_rate > 1:
-            # faster means output is smaller -> padding
-            pad_l = int((max_length - istft.shape[0])/2)
-            pad_r = max_length - (pad_l + istft.shape[0])
-            return F.pad(istft, (pad_l, pad_r))
-        else:
-            # slower means longer -> chopping of
-            low = int((istft.shape[0] - max_length)/2)
-            return istft[low:low+max_length]
+        return AF.istft(vocoded, n_fft.item(), hop_length=hop_length).squeeze()
