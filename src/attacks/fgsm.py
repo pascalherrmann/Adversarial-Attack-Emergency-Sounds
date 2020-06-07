0c0853f633fef339b5059f8b1a4c109305f44550
Yan Scholten <yascho@mail.upb.de>
2020-06-08 00:08:27 +0200

    improving attacks

--- src/attacks/fgsm.py
+++ src/attacks/fgsm.py
@@ -4,14 +4,18 @@ import torch.nn.functional as F
 
 class FGSM(Attack):
     
-    def attackSample(self, x, y, epsilon=0):
+    def attackSample(self, x, y, norm='inf', epsilon=0):
         x.requires_grad_()
         
         loss = F.nll_loss(self.model(x), y)
         self.model.zero_grad()
         loss.backward()
 
-        x = x + epsilon * x.grad.data.sign()
+        if norm == "inf":
+            x_pert = x + epsilon * x.grad.data.sign()
+        else:
+            normed_grad = x.grad.data.norm(p=float(norm), dim=[2,3]).unsqueeze(2).unsqueeze(3)
+            x_pert = x + epsilon * x.grad.data/normed_grad
 
         # projection in case epsilon is too large
         x = x.clamp(-1, 1).detach()  
