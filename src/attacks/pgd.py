2ccf5e3e61aa805c24e9bcbe6dd0896c5b6204ee
Yan Scholten <yascho@mail.upb.de>
2020-06-09 13:09:15 +0200

    YS

--- src/attacks/pgd.py
+++ src/attacks/pgd.py
@@ -6,14 +6,14 @@ class PGD(Attack):
     
     def attackSample(self, x, y, epsilon=0, num_iter=1):
         for i in range(num_iter):
-            x.requires_grad_()
+            x[0].requires_grad_()
             loss = F.nll_loss(self.model(x), y)
             self.model.zero_grad()
             loss.backward()
 
-            x = x + epsilon * x.grad.data
+            x[0] = x[0] + epsilon * x[0].grad.data
 
             # projection in case epsilon is too large
-            x = x.clamp(-1, 1).detach() 
+            x[0] = x[0].clamp(-1, 1).detach() 
 
         return x 
