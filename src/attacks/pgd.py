bdd8b28a17278f7a457a85b9da5e4a82567d40c4
Yan Scholten <yascho@mail.upb.de>
2020-06-11 17:15:14 +0200

    YS

--- src/attacks/pgd.py
+++ src/attacks/pgd.py
@@ -8,7 +8,7 @@ class PGD(Attack):
     def __init__(self, **params):
         self.fga = FastGradientAttack(**params)
     
-    def attackSample(self, x, y, epsilon=0, num_iter=1, norm='inf', loss_fn=F.nll_loss)
+    def attackSample(self, x, y, epsilon=0, num_iter=1, norm='inf', loss_fn=F.nll_loss):
         raise Exception("Not vectorized - TODO")
         for i in range(num_iter):
             x = self.fga.attackSample(x, y, epsilon, norm, loss_fn)
