40a27fba3501d5ba6dba3ac6c031d05ad5a0b826
Yan Scholten <yascho@mail.upb.de>
2020-06-11 17:16:37 +0200

    YS

--- src/attacks/pgd.py
+++ src/attacks/pgd.py
@@ -5,8 +5,13 @@ import torch.nn.functional as F
 
 class PGD(Attack):
 
-    def __init__(self, **params):
-        self.fga = FastGradientAttack(**params)
+    def __init__(self, model, data_loader,
+                    attack_parameters, early_stopping=-1,
+                    device='cuda', save_samples=True
+
+        self.fga=FastGradientAttack(model, data_loader,
+                                    attack_parameters, early_stopping=early_stopping,
+                                    device=device, save_samples=save_samples)
     
     def attackSample(self, x, y, epsilon=0, num_iter=1, norm='inf', loss_fn=F.nll_loss):
         raise Exception("Not vectorized - TODO")
