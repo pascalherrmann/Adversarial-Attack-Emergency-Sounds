4c5afa4499dfc51848f19eb5b354d2432eee447e
Yan Scholten <yascho@mail.upb.de>
2020-06-11 15:02:13 +0200

    YS

--- src/attacks/attack.py
+++ src/attacks/attack.py
@@ -33,6 +33,8 @@ class Attack(ABC):
 
             # we only attack correctly classified samples (TPs and TNs)  
             samples_to_attack = (y_initial == y_true)
+            if len(batch['audio']) == 1 and samples_to_attack.sum() == 0:
+                continue
             y_initial = y_initial[samples_to_attack]
             y_true = y_true[samples_to_attack]
             x_to_perturb = {k: x[k][samples_to_attack] for k in x}
