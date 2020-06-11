77ac38ccebdef49605e33954198a2ec11133f333
Yan Scholten <yascho@mail.upb.de>
2020-06-11 17:29:20 +0200

    YS

--- src/attacks/pgd.py
+++ src/attacks/pgd.py
@@ -18,10 +18,10 @@ class ProjectedGradientDescent(Attack):
             loss.backward()
 
             if norm == "inf":
-            x['audio'] = x['audio'] + epsilon * x['audio'].grad.sign()
+                x['audio'] = x['audio'] + epsilon * x['audio'].grad.sign()
             else:
-            normed_grad = x.grad.norm(p=float(norm), dim=[2]).unsqueeze(2)
-            x['audio'] = x['audio'] + epsilon * x.grad/normed_grad
+                normed_grad = x.grad.norm(p=float(norm), dim=[2]).unsqueeze(2)
+                x['audio'] = x['audio'] + epsilon * x.grad/normed_grad
 
             # projection in case epsilon is too large
             x['audio'] = x['audio'].clamp(-1, 1).detach()
