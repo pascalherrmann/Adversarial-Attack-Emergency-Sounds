338a0870c31b48be800f59d27a50cb84195c5886
Yan Scholten <yascho@mail.upb.de>
2020-06-23 17:26:37 +0200

    YS

--- src/attacks/functionalPitch.py
+++ src/attacks/functionalPitch.py
@@ -1,31 +1,37 @@
 from attacks.attack import Attack
 
+from torchaudio import functional as AF
 import torch.nn.functional as F
+import torchaudio
+import math
 import torch
+import librosa
+import numpy as np
 
-class FunctionalVolumeAttack(Attack):
+class FunctionalPitchAttack(Attack):
 
-    '''
-        - Epsilon step in gradient direction (step size defined by norm)
-        - Fully vectorized 
-        - For num_iter=1 and norm='inf': Degrades to Fast Gradient Sign Method (FGSM)
-    '''
-    def attackSample(self, x, y, epsilonVolume=0, epsilonPGD=0, num_iter=1,
+    def attackSample(self, x, y, num_iter_stretch=1, lower_stretch=-1, upper_stretch=1,
+                     epsilon=0, num_iter=1, norm='inf', loss_fn=F.nll_loss, lower=0.2, upper=2):
+        
+        x = self.pitch_attack(x, y, num_iter_stretch, lower, upper)
+        x = self.noise_attack(x, y, epsilon, num_iter, norm, loss_fn, lower, upper)
+        
+        return x
+    
+    def noise_attack(self, x, y, epsilon=0, num_iter=1,
                         norm='inf', loss_fn=F.nll_loss, lower=0.2, upper=2):
         
         batch_size = x['audio'].size(0)
         clip_length = x['audio'].size(1)
         
-        a = torch.ones(batch_size).unsqueeze(1).to(self.device)
         delta = torch.zeros(batch_size, clip_length).to(self.device)
         
         for _ in range(num_iter):
            
-            a.requires_grad_()
             delta.requires_grad_()
             
             tmp_audio = x['audio']
-            x['audio'] = (a * x['audio'] + delta).clamp(-1, 1)
+            x['audio'] = (x['audio'] + delta).clamp(-1, 1)
             
             loss = loss_fn(self.model(x), y)
             self.model.zero_grad()
@@ -34,20 +40,36 @@ class FunctionalVolumeAttack(Attack):
             x['audio'] = tmp_audio
 
             if norm == "inf":
-                a = a + epsilonVolume * a.grad.sign()
-                a = a.clamp(lower, upper).detach()
-                
-                delta = delta + epsilonPGD * delta.grad.sign()
+                delta = delta + epsilon * delta.grad.sign()
                 delta = delta.detach()
             else:
-                a = a + epsilonVolume * a.grad/a.grad.norm(p=foat(norm), dim=[1]).unsqueeze(1)
-                a = a.clamp(lower, upper).detach()
-                delta = delta + epsilonPGD * delta.grad/delta.grad.norm(p=foat(norm), dim=[1]).unsqueeze(1)
+                delta = delta + epsilon * delta.grad/delta.grad.norm(p=foat(norm), dim=[1]).unsqueeze(1)
                 delta = delta.detach()
             
-        x['audio'] = (a * x['audio'] + delta).clamp(-1, 1)
+        x['audio'] = (x['audio'] + delta).clamp(-1, 1)
         return x
+    
+    def pitch_attack(self, x, y, num_iter=1, lower=-1, upper=1):
+        n_steps_search_range = torch.arange(lower, upper, (upper-lower)/num_iter)
+        losses = []
+        stretched_inputs = []
+        
+        with torch.no_grad():
+            x_original = x['audio']
+            
+            for n_steps in n_steps_search_range:
+                # we expect full batch to have the same sample rate here
+                x['audio'] = self.pitch_shift(x_original.clone(), sr=x['sample_rate'][0], n_steps=n_steps)
+                stretched_inputs.append(x['audio'])
+                losses.append(F.nll_loss(self.model(x), y, reduction='none'))
+                 
+        best_rates = torch.stack(losses).argmax(0)
 
+        for x_i in range(len(best_rates)): # here is potential to further vectorize 
+            x['audio'][x_i] = stretched_inputs[best_rates[x_i]][x_i].clamp(-1, 1)
+        
+        return x
+    
     def pitch_shift(self, batch, sr, n_steps, bins_per_octave=12):
         # https://librosa.github.io/librosa/_modules/librosa/effects.html#pitch_shift
         assert bins_per_octave > 1 or not np.issubdtype(type(bins_per_octave), np.integer)
