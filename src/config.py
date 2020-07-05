ca5ab97e43ba50f7f9887c21b6491e3ecbd37c03
Yan Scholten <yan.scholten@tum.de>
2020-07-05 18:36:20 +0200

    debugging CRNN

--- src/config.py
+++ src/config.py
@@ -1,10 +1,10 @@
 # Emergency Dataset
-DATASET1_DATA_8K_DIR = "/nfs/students/summer-term-2020/project-4/data/dataset1/dataset_8k"
-DATASET1_DATA_48K_DIR = "/nfs/students/summer-term-2020/project-4/data/dataset1/dataset_48k"
+DATASET1_DATA_8K_DIR = "/nfs/students/summer-term-2020/project-4/data/dataset1/dataset_8k/"
+DATASET1_DATA_48K_DIR = "/nfs/students/summer-term-2020/project-4/data/dataset1/dataset_48k/"
 
 # Control dataset
-DATASET2_DATA_8K_DIR = "" # TODO
-DATASET2_DATA_48K_DIR = "" # TODO
+DATASET2_DATA_8K_DIR = "/nfs/students/summer-term-2020/project-4/data/dataset2/dataset_8k/"
+DATASET2_DATA_48K_DIR = "/nfs/students/summer-term-2020/project-4/data/dataset2/dataset_48k/"
 
 # default (emergency dataset)
 DATA_8K_DIR = DATASET1_DATA_8K_DIR
@@ -12,12 +12,12 @@ DATA_48K_DIR = DATASET1_DATA_48K_DIR
 
 # dataset paths as dict 
 DATASETS_DIR = {
-		'0': {'8000': DATASET1_DATA_8K_DIR, '48000': DATASET1_DATA_48K_DIR},
-	 	'1': {'8000': DATASET2_DATA_8K_DIR, '48000': DATASET2_DATA_48K_DIR}
+		0: {8000: DATASET1_DATA_8K_DIR, 48000: DATASET1_DATA_48K_DIR},
+	 	1: {8000: DATASET2_DATA_8K_DIR, 48000: DATASET2_DATA_48K_DIR}
 	}
 
-DATASET_EMERGENCY_ID = '0'
-DATASET_CONTROL_ID = '1'
+DATASET_EMERGENCY = 0
+DATASET_CONTROL = 1
 
 # Further paths
 LOG_DIR = "/nfs/students/summer-term-2020/project-4/logs"
