c6e866c5ee43f4db0cfc40517b0736fddbadba75
Yan Scholten <yascho@mail.upb.de>
2020-07-05 19:57:35 +0200

    YS

--- src/datasets/dataset.py
+++ src/datasets/dataset.py
@@ -33,12 +33,7 @@ class Dataset(Dataset):
         audio = self.dataset[index]['data'][0]
         if self.fixed_padding:
             assert (self.max_length_sample - len(audio)) >= 0
-            try:
-                audio = F.pad(audio, (0, self.max_length_sample - len(audio)), mode='constant', value=0)
-            except AttributeError:
-                print(audio)
-                print(self.max_length_sample)
-                print(len(audio))
+            audio = F.pad(audio, (0, self.max_length_sample - len(audio)), mode='constant', value=0)
 
         return {'audio': audio, 
                 'sample_rate': self.sample_rate, 
