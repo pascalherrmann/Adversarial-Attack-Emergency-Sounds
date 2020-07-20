171831ad6b6fc1eda529d2d167eb72c59e42f9bb
Yan Scholten <yascho@mail.upb.de>
2020-07-20 12:42:42 +0200

    ablation study

--- src/classification/models/AblationModel.py
+++ src/classification/models/AblationModel.py
@@ -1,14 +1,3 @@
-'''
-hparams: = {'batch_size': 16, 
-            'learning_rate': 0.0009471138112165006, 
-            'p_dropout': 0.3394112556659779, 
-            'n_hidden': 711, 
-            'lr_decay': 0.7514824092200452, 
-            'weight_decay': 0.003018912473366329}
--> spectrogramxxx_v359_acc_0.8998_best.p
-'''
-
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -17,38 +6,40 @@ import torchaudio
 
 from classification.trainer.GeneralPLModule import GeneralPLModule
 
-class MelSpectrogramCNN_8K(nn.Module):
+class AblationModel(nn.Module):
     
     def __init__(self, hparams):
-        super(MelSpectrogramCNN_8K, self).__init__()
+        super(AblationModel, self).__init__()
         self.datasets = {}
+        
+        self.normal_spectrogram = hparams["normal_spectrogram"]
         
         self.windowsize = 800
         self.window = torch.hann_window(self.windowsize).cuda()
         
         self.convs = nn.Sequential(
                     nn.BatchNorm2d(1),
-                    nn.Conv2d(1, 10, kernel_size=10,stride=2),
-                    nn.BatchNorm2d(10),
-                    nn.PReLU(),
-                    nn.MaxPool2d(kernel_size=2, stride=2),
-                    nn.Dropout(p=hparams["p_dropout"], inplace=False),
-            
-                    nn.Conv2d(10, 20, kernel_size=10,stride=1),
+                    nn.Conv2d(1, 20, kernel_size=10,stride=2),
                     nn.BatchNorm2d(20),
                     nn.PReLU(),
                     nn.MaxPool2d(kernel_size=2, stride=2),
                     nn.Dropout(p=hparams["p_dropout"], inplace=False),
-                        ## errorous
-                    nn.Conv2d(20, 40, kernel_size=5,stride=1),
+
+                    nn.Conv2d(20, 40, kernel_size=10,stride=1),
                     nn.BatchNorm2d(40),
+                    nn.PReLU(),
+                    nn.MaxPool2d(kernel_size=2, stride=2),
+                    nn.Dropout(p=hparams["p_dropout"], inplace=False),
+
+                    nn.Conv2d(40, 80, kernel_size=5,stride=1),
+                    nn.BatchNorm2d(80),
                     nn.PReLU(),
                     nn.MaxPool2d(kernel_size=2, stride=2),
                     nn.Dropout(p=hparams["p_dropout"], inplace=False),
                 )
         
         self.dense = nn.Sequential(                 
-                        nn.Linear(2400, hparams["n_hidden"]),
+                        nn.Linear(80, hparams["n_hidden"]),
                         nn.PReLU(),
                         nn.Dropout(p=hparams["p_dropout"], inplace=False),
                         nn.Linear(hparams["n_hidden"], 2)  
@@ -56,19 +47,23 @@ class MelSpectrogramCNN_8K(nn.Module):
 
     def forward(self, batch):
         x = batch['audio']
-        x = torchaudio.transforms.MelSpectrogram().cuda()(x)
-        # debug
+        if self.normal_spectrogram:
+            x = torchaudio.transforms.Spectrogram().cuda()(x)
+        else:
+            x = torchaudio.transforms.MelSpectrogram().cuda()(x)
+
         x = x.unsqueeze(1).float()
         x = self.convs(x)
-        x = x.view(x.shape[0], -1)
+        x = x.view(x.shape[0], x.shape[1], -1)
+        x = F.avg_pool1d(x, kernel_size=x.size()[2:]).squeeze(2)
         x = self.dense(x)
         return F.log_softmax(x,dim=1)
 
-class MelSpectrogramCNN_8KPLModule(GeneralPLModule):
+class AblationModelPLModule(GeneralPLModule):
 
     def __init__(self, hparams):
         super().__init__(hparams)
-        self.model = MelSpectrogramCNN_8K(hparams)
+        self.model = AblationModel(hparams)
         
     def dataset_info(self):
         dataset_type = {"sample_rate": 8000}
